8e3cfd4a343f639a14215008f4785adf6d457493
Michiel <michielp1807@gmail.com>
2024-09-04 12:51:08 +0200

    Fix typo
    Signed-off-by: Michiel <michielp1807@gmail.com>

--- examples/cli-test-cross-type.sh
+++ examples/cli-test-cross-type.sh
@@ -2,8 +2,8 @@
 HPKE="./build-hpke"
 NACL="./build-nacl"
 
-HPKE_TPS="$HPKE/bin/tsp"
-NACL_TPS="$NACL/bin/tsp"
+HPKE_TSP="$HPKE/bin/tsp"
+NACL_TSP="$NACL/bin/tsp"
 
 # install two different versions of the TSP command line example tool
 cargo install --path . --bin tsp --root "$HPKE"
@@ -17,31 +17,31 @@ echo "---- cleanup the database"
 rm -f marlon.sqlite marc.sqlite
 
 echo "---- create a new sender identity"
-$HPKE_TPS --database marlon create --alias marlon `randuser`
+$HPKE_TSP --database marlon create --alias marlon `randuser`
 
 echo "---- create a new receiver identity"
-$NACL_TPS --database marc create --alias marc `randuser`
+$NACL_TSP --database marc create --alias marc `randuser`
 
-DID_MARC=$($NACL_TPS --database marc print marc)
-DID_MARLON=$($HPKE_TPS --database marlon print marlon)
+DID_MARC=$($NACL_TSP --database marc print marc)
+DID_MARLON=$($HPKE_TSP --database marlon print marlon)
 
 echo "---- verify the address of the receiver"
-$HPKE_TPS --database marlon verify --alias marc "$DID_MARC"
+$HPKE_TSP --database marlon verify --alias marc "$DID_MARC"
 
 echo "---- verify the address of the sender"
-$NACL_TPS --database marc verify --alias marlon "$DID_MARLON"
+$NACL_TSP --database marc verify --alias marlon "$DID_MARLON"
 
 echo "---- wait 2 seconds and then send a message to the receiver"
-sleep 2 && echo "Oh hi Marc" | $HPKE_TPS --database marlon send -s marlon -r marc &
+sleep 2 && echo "Oh hi Marc" | $HPKE_TSP --database marlon send -s marlon -r marc &
 
 echo "---- receive the message"
-$NACL_TPS --database marc receive --one marc
+$NACL_TSP --database marc receive --one marc
 
 echo "---- wait 1 seconds and then send a message back"
-sleep 1 && echo "Oh hello Marlon" | $NACL_TPS --database marc send -s marc -r marlon &
+sleep 1 && echo "Oh hello Marlon" | $NACL_TSP --database marc send -s marc -r marlon &
 
 echo "---- receive the message"
-$HPKE_TPS --database marlon receive --one marlon
+$HPKE_TSP --database marlon receive --one marlon
 
 echo "---- cleanup databases"
 rm -f marc.sqlite marlon.sqlite
