56fafa0759fd88e5f83dab62ce31ed9ed082dda9
Michiel <michielp1807@gmail.com>
2025-04-03 16:10:01 +0200

    Rename end-points to a and b
    Signed-off-by: Michiel <michielp1807@gmail.com>

--- examples/cli-demo-routed.sh
+++ examples/cli-demo-routed.sh
@@ -12,7 +12,7 @@ cointoss() {
 }
 
 echo "---- cleanup the database"
-rm -f marlon.sqlite marc.sqlite p.sqlite q.sqlite
+rm -f a.sqlite b.sqlite p.sqlite q.sqlite
 
 echo
 echo "==== create sender, receiver, and intermediaries"
@@ -24,7 +24,7 @@ else
     echo "------ drop off vid will be a nested vid (out-of-band)"
 fi
 
-for entity in marlon p q $Q2 marc; do
+for entity in a p q $Q2 b; do
     if cointoss; then
 	echo "------ $entity (identifier for ${entity%%[0-9]*}) uses did:web"
 	tsp --database "${entity%%[0-9]*}" create --alias $entity `randuser`
@@ -39,11 +39,11 @@ for entity in marlon p q $Q2 marc; do
 	fi
     fi
 done
-DID_MARLON=$(tsp --database marlon print marlon)
+DID_A=$(tsp --database a print a)
 DID_P=$(tsp --database p print p)
 DID_Q=$(tsp --database q print q)
 [ "$Q2" ] && DID_Q2=$(tsp --database q print q2)
-DID_MARC=$(tsp --database marc print marc)
+DID_B=$(tsp --database b print b)
 
 wait
 sleep 5
@@ -51,17 +51,17 @@ echo
 echo "==== let the nodes introduce each other"
 
 echo "---- verify the address of the receiver"
-tsp --database marlon verify --alias marc "$DID_MARC"
+tsp --database a verify --alias b "$DID_B"
 
 echo "---- establish outer relation a<->p"
-tsp --database marlon verify --alias p "$DID_P"
+tsp --database a verify --alias p "$DID_P"
 
-sleep 2 && tsp --database marlon request -s marlon -r p &
+sleep 2 && tsp --database a request -s a -r p &
 read -d '\t' vid thread_id <<< $(tsp --yes --database p receive --one p)
 
-tsp --database p set-alias marlon "$vid"
-sleep 2 && tsp --database p accept -s p -r marlon --thread-id "$thread_id" &
-tsp --database marlon receive --one marlon
+tsp --database p set-alias a "$vid"
+sleep 2 && tsp --database p accept -s p -r a --thread-id "$thread_id" &
+tsp --database a receive --one a
 
 echo "---- establish outer relation p<->q"
 tsp --database q verify --alias p "$DID_P"
@@ -75,48 +75,48 @@ tsp --database q receive --one q
 
 if [ "$Q2" ]; then
     echo "---- establish outer relation q2<->b"
-    tsp --database marc verify --alias q2 "$DID_Q2"
+    tsp --database b verify --alias q2 "$DID_Q2"
 
-    sleep 2 && tsp --database marc request -s marc -r q2 &
+    sleep 2 && tsp --database b request -s b -r q2 &
     read -d '\t' vid thread_id <<< $(tsp --yes --database q receive --one q2)
-    tsp --database q set-alias marc "$vid"
+    tsp --database q set-alias b "$vid"
 
-    sleep 2 && tsp --database q accept -s q2 -r marc --thread-id "$thread_id" &
-    tsp --database marc receive --one marc
+    sleep 2 && tsp --database q accept -s q2 -r b --thread-id "$thread_id" &
+    tsp --database b receive --one b
 
-    tsp --database q set-relation q2 marc
+    tsp --database q set-relation q2 b
 else
     echo "---- establish outer relation q<->b"
-    tsp --database marc verify --alias q "$DID_Q"
+    tsp --database b verify --alias q "$DID_Q"
 
-    sleep 2 && tsp --database marc request -s marc -r q &
+    sleep 2 && tsp --database b request -s b -r q &
     read -d '\t' vid thread_id <<< $(tsp --yes --database q receive --one q)
-    tsp --database q set-alias marc "$vid"
+    tsp --database q set-alias b "$vid"
 
-    sleep 2 && tsp --database q accept -s q -r marc --thread-id "$thread_id" &
-    tsp --database marc receive --one marc
+    sleep 2 && tsp --database q accept -s q -r b --thread-id "$thread_id" &
+    tsp --database b receive --one b
 
     echo "---- establish nested relation q<->b"
 
-    sleep 2 && tsp --database marc request --nested -s marc -r q > /dev/null &
-    read -d '\t' nested_marc thread_id <<< $(tsp --database q receive --one q)
+    sleep 2 && tsp --database b request --nested -s b -r q > /dev/null &
+    read -d '\t' nested_b thread_id <<< $(tsp --database q receive --one q)
 
-    sleep 2 && tsp --database q accept --nested -s q -r "$nested_marc" --thread-id "$thread_id" > /tmp/vid &
-    DID_Q2=$(tsp --database marc receive --one marc)
+    sleep 2 && tsp --database q accept --nested -s q -r "$nested_b" --thread-id "$thread_id" > /tmp/vid &
+    DID_Q2=$(tsp --database b receive --one b)
 fi
 
 echo "---- setup the route"
-tsp --database marlon set-route marc "p,$DID_Q,$DID_Q2"
+tsp --database a set-route b "p,$DID_Q,$DID_Q2"
 
 wait
 sleep 5
 echo
 echo "==== send a routed message"
 
-sleep 2 && echo -n "Indirect Message from Marlon to Marc was received!" | tsp --database marlon send -s marlon -r marc &
+sleep 2 && echo -n "Indirect Message from A to B was received!" | tsp --database a send -s a -r b &
 tsp --yes --database p receive --one p &
 tsp --yes --database q receive --one q &
-tsp --yes --database marc receive --one marc
+tsp --yes --database b receive --one b
 
 echo "---- cleanup databases"
-rm -f marlon.sqlite marc.sqlite p.sqlite q.sqlite
+rm -f a.sqlite b.sqlite p.sqlite q.sqlite
