fb792b84d5cc0c47166dc3d1d3f32318da710ee4
Maximilian Pohl <maximilian@tweedegolf.com>
2025-04-29 16:30:00 +0200

    Add documentation on how to create DID webvh
    Signed-off-by: Maximilian Pohl <maximilian@tweedegolf.com>

--- examples/src/cli.rs
+++ examples/src/cli.rs
@@ -9,11 +9,16 @@ use tokio::io::AsyncReadExt;
 use tracing::{debug, error, info, trace};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use tsp_sdk::{
+    cesr::{self, Part}, AskarSecureStorage, AsyncSecureStore, Error, ExportVid, OwnedVid,
+    ReceivedTspMessage, SecureStorage, VerifiedVid,
+    Vid,
+};
+use tsp_sdk::vid::vid_to_did_document;
+use tsp_sdk::{
     AskarSecureStorage, AsyncSecureStore, Error, ExportVid, OwnedVid, ReceivedTspMessage,
     SecureStorage, VerifiedVid, Vid,
     cesr::{self, Part},
 };
-use tsp_sdk::vid::vid_to_did_document;
 
 #[derive(Debug, Parser)]
 #[command(name = "tsp", version)]
@@ -63,7 +68,9 @@ enum Commands {
         sender: Option<String>,
     },
     #[command(arg_required_else_help = true)]
-    Print { alias: String },
+    Print {
+        alias: String,
+    },
     #[command(
         arg_required_else_help = true,
         about = "create and register a did:web identifier"
@@ -72,8 +79,12 @@ enum Commands {
         username: String,
         #[arg(short, long)]
         alias: Option<String>,
-        #[arg(short, long, help = "Do not publish the DID on the DID server but print it to the console")]
-        local: bool
+        #[arg(
+            short,
+            long,
+            help = "Do not publish the DID on the DID server but print it to the console"
+        )]
+        local: bool,
     },
     CreatePeer {
         alias: String,
@@ -98,13 +109,25 @@ enum Commands {
         vid: String,
     },
     #[command(arg_required_else_help = true)]
-    SetAlias { alias: String, vid: String },
+    SetAlias {
+        alias: String,
+        vid: String,
+    },
     #[command(arg_required_else_help = true)]
-    SetRoute { vid: String, route: String },
+    SetRoute {
+        vid: String,
+        route: String,
+    },
     #[command(arg_required_else_help = true)]
-    SetParent { vid: String, other_vid: String },
+    SetParent {
+        vid: String,
+        other_vid: String,
+    },
     #[command(arg_required_else_help = true)]
-    SetRelation { vid: String, other_vid: String },
+    SetRelation {
+        vid: String,
+        other_vid: String,
+    },
     #[command(arg_required_else_help = true, about = "send a message")]
     Send {
         #[arg(short, long, required = true)]
@@ -308,11 +331,16 @@ async fn run() -> Result<(), Error> {
 
             print!("{vid}");
         }
-        Commands::Create { username, alias, local } => {
+        Commands::Create {
+            username,
+            alias,
+            local,
+        } => {
             let did = format!(
                 "did:web:{}:endpoint:{username}",
                 did_server.replace(":", "%3A")
-            .replace("/", ":"));
+            .replace("/", ":")
+            );
 
             if let Some(alias) = alias {
                 vid_wallet.set_alias(alias.clone(), did.clone())?;
@@ -360,11 +388,11 @@ async fn run() -> Result<(), Error> {
                     }
                 };
                 info!(
-                "published DID document at {}",
-                tsp_sdk::vid::did::get_resolve_url(&did)
-                    .unwrap()
-                    .to_string()
-            );
+                    "published DID document at {}",
+                    tsp_sdk::vid::did::get_resolve_url(&did)
+                        .unwrap()
+                        .to_string()
+                );
             }
 
             vid_wallet.add_private_vid(private_vid.clone())?;
@@ -397,7 +425,7 @@ async fn run() -> Result<(), Error> {
 
             info!("created identity from file {}", private_vid.identifier());
         }
-        Commands::ExportPiv { vid} => {
+        Commands::ExportPiv { vid } => {
             let exported = vid_wallet.export()?.into_iter().find(|e| e.id == vid);
             println!("{}", serde_json::to_string_pretty(&exported).unwrap());
         }
