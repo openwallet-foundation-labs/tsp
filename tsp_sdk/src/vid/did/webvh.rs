a003e7860490f7fa3f69bf09da1dff3acce088b8
Maximilian Pohl <maximilian@tweedegolf.com>
2025-04-29 10:19:48 +0200

    First working version
    Signed-off-by: Maximilian Pohl <maximilian@tweedegolf.com>

--- tsp_sdk/src/vid/did/webvh.rs
+++ tsp_sdk/src/vid/did/webvh.rs
@@ -1,8 +1,8 @@
 use crate::Vid;
 use crate::vid::VidError;
+use crate::vid::did::web::{DidDocument, resolve_document};
 use didwebvh_resolver;
 use didwebvh_resolver::{DefaultHttpClient, ResolutionOptions};
-use crate::vid::did::web::{resolve_document, DidDocument};
 
 pub(crate) const SCHEME: &str = "webvh";
 
@@ -12,6 +12,6 @@ pub async fn resolve(id: &str) -> Result<Vid, VidError> {
     let resolved = resolver.resolve(id, &ResolutionOptions::default()).await?;
 
     let did_doc: DidDocument = serde_json::from_value(resolved.did_document)?;
-    
+
     resolve_document(did_doc, id)
 }
