b0c3e17b9fa7b75646908419eee1efb28f3251c7
Michiel <michielp1807@gmail.com>
2025-05-01 11:03:04 +0200

    Update example.py
    Signed-off-by: Michiel <michielp1807@gmail.com>

--- tsp_python/example.py
+++ tsp_python/example.py
@@ -5,15 +5,17 @@ import requests
 store = tsp.SecureStore()
 
 # Create identity
+print("Creating identity")
 alice_username = "alice" + str(random.randint(0, 999999))
-print(f"Username: {alice_username}")
+print(f"> Username: {alice_username}")
 
 alice = tsp.OwnedVid.bind(
-    f"did:web:did.teaspoon.world:user:{alice_username}",  # my DID
-    f"https://demo.teaspoon.world/user/{alice_username}",  # transport URL
+    f"did:web:did.teaspoon.world:endpoint:{alice_username}",  # my DID
+    f"https://demo.teaspoon.world/endpoint/{alice_username}",  # transport URL
 )
 
 # Publish DID (this is non-standard and dependents on the implementation of the DID support server)
+print("Publishing DID")
 response = requests.post(
     "https://did.teaspoon.world/add-vid",
     data=alice.json(),
@@ -26,11 +28,13 @@ if not response.ok:
 
 store.add_private_vid(alice)
 
-# Resolve other party
-bob_did = "did:web:did.teaspoon.world:user:bob"
+# Resolve other party (may fail if endpoint bob does not exist)
+print("Resolve other party")
+bob_did = "did:web:did.teaspoon.world:endpoint:bob"
 endpoint = store.resolve_did_web(bob_did)
-print("bob's endpoint:", endpoint)
+print("> Bob's endpoint:", endpoint)
 
 # Send a message
+print("Send a message")
 response = store.send_message(alice.identifier(), bob_did, b"hi bob")
-print(response.status_code)
+print(f"Success: {response.ok} ({response.status_code})")
