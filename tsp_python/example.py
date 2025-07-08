28bbe2e97a1978818ac750e5ae8f82dc1f580e1d
Michiel <michielp1807@gmail.com>
2025-07-08 10:28:19 +0200

    Fix Python example
    Signed-off-by: Michiel <michielp1807@gmail.com>

--- tsp_python/example.py
+++ tsp_python/example.py
@@ -6,35 +6,38 @@ store = tsp.SecureStore()
 
 # Create identity
 print("Creating identity")
-alice_username = "alice" + str(random.randint(0, 999999))
-print(f"> Username: {alice_username}")
-
-alice = tsp.OwnedVid.bind(
-    f"did:web:did.teaspoon.world:endpoint:{alice_username}",  # my DID
-    f"https://demo.teaspoon.world/endpoint/{alice_username}",  # transport URL
-)
+username = "alice" + str(random.randint(0, 999999))
+did = "did:web:did.teaspoon.world:endpoint:" + username
+transport_url = "https://p.teaspoon.world/endpoint/" + did
+alice = tsp.OwnedVid.bind(did, transport_url)
 
 # Publish DID (this is non-standard and dependents on the implementation of the DID support server)
-print("Publishing DID")
+print("Publishing DID: " + did)
 response = requests.post(
     "https://did.teaspoon.world/add-vid",
     data=alice.json(),
     headers={"Content-type": "application/json"},
 )
 if not response.ok:
-    raise Exception(
-        f"Could not publish DID (status code: {response.status_code}):\n{alice.json()}"
-    )
+    raise Exception(f"Could not publish DID (status code: {response.status_code})")
 
+# Save DID in wallet
 store.add_private_vid(alice)
 
 # Resolve other party (may fail if endpoint bob does not exist)
-print("Resolve other party")
 bob_did = "did:web:did.teaspoon.world:endpoint:bob"
-endpoint = store.resolve_did_web(bob_did)
-print("> Bob's endpoint:", endpoint)
+print("Resolving DID: " + bob_did)
+bob_endpoint = store.verify_vid(bob_did)
+print("> Bob's endpoint:", bob_endpoint)
 
 # Send a message
-print("Send a message")
-response = store.send_message(alice.identifier(), bob_did, b"hi bob")
-print(f"Success: {response.ok} ({response.status_code})")
+print("Sending message...")
+store.send(alice.identifier(), bob_did, b"hi bob")
+
+# Receive messages
+print("Listening for messages...")
+while True:
+    response = store.receive(alice.identifier())
+    print("Received: " + str(response))
+    if isinstance(response, tsp.GenericMessage):
+        print("> " + str(response.message))
